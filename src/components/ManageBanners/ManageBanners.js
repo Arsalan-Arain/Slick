fd42ff911ff8caf1d6f4bf001a351412260a256c
Arsalan Ali <arsalanali.arain1@gmail.com>
2021-09-12 14:22:18 +0500

    added del confirmation modal in banner

--- src/components/ManageBanners/ManageBanners.js
+++ src/components/ManageBanners/ManageBanners.js
@@ -7,32 +7,42 @@ import * as actionCreators from '../../redux/actions/index';
 import Modal from '../UI/Modal/Modal';
 import Form from '../UI/Form/FormUpdateBanner/FormUpdateBanner';
 import linkIcon from '../../assets/linkIcon.svg'
+import DeleteConfirm from '../UI/DeleteConfirm/DeleteConfirm';
 
 const manageBanners = (props) => {
   const [showModal, setShowModal] = useState(false);
+  const [showDelModal, setShowDelModal] = useState(false);
   const [bannerId, setBannerId] = useState();
   const [bannerLink, setBannerLink] = useState();
 
-  const modalHandler = () => {
-    setShowModal(true);
-  }
   const closeModalHandler = () => {
     setShowModal(false);
+    setShowDelModal(false);
   }
   const saveItemHandler = (updatedBanner) => {
     props.onUpdateBanner(bannerId, updatedBanner)
     closeModalHandler()
   }
 
+  const delConfirmHandler = () => {
+    props.onDeleteBanner(bannerId);
+    closeModalHandler();
+  }
+
+  const delBannerHandler = (id) => {
+    setBannerId(id);
+    setShowDelModal(true)
+  }
   const updateBannerHandler = (id, link) => {
     setBannerId(id)
     setBannerLink(link);
-    modalHandler();
+    setShowModal(true)
   }
 
   return (
     <React.Fragment>
       <Modal title={'Update Banner'} show={showModal} modalClosed={closeModalHandler}><Form url={bannerLink} clicked={saveItemHandler} /></Modal>
+      <Modal title={'Delete Banner'} show={showDelModal} modalClosed={closeModalHandler}><DeleteConfirm item={'Banner'} clicked={delConfirmHandler} /></Modal>
       <div className={classes.nestedPageBody}>
 
         {props.banners.map(banner => (
@@ -42,7 +52,7 @@ const manageBanners = (props) => {
             </div>
             <div className={classes.productLink}><img src={linkIcon} alt="" /><p style={{overflow:'hidden', textOverflow:'ellipsis', whiteSpace:'nowrap'}}>{banner.link}</p></div>
             <img onClick={() => { updateBannerHandler(banner._id, banner.link) }} src={editBtn} alt='btn' className={classes.smallBtn} />
-            <img onClick={() => props.onDeleteBanner(banner._id)} src={deltBtn} alt='btn' className={classes.smallBtn} />
+            <img onClick={() => delBannerHandler(banner._id)} src={deltBtn} alt='btn' className={classes.smallBtn} />
           </div>
         ))}
       </div>
